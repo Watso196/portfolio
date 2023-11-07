cd5706fe2b13702575b6d00e8c21a39df29e349d
Kalib Watson <kwatson@kwatson-mac-2155.local>
2023-11-07 09:48:13 -0500

    linting: updating linting rules and packages

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ const App = () => {
         <>
             <h1>Hello React</h1>
             <p>Hi there! This is my first webpack site I'm building.</p>
+            <div role="button">button</div>
         </>
     );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   "output": {
     "path": path.join(__dirname, "/dist"),
     "filename": "[name].js",
-    sourceMapFilename: "bundle.js.map"
+    sourceMapFilename: "[file].map[query]"
   },
   plugins: [
     new HtmlWebpackPlugin({
