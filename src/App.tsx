3c4bd7ab251c1518350bcfc735ae958d18424d5f
Kalib Watson <kwatson@kwatson-mac-2155.local>
2023-11-07 10:48:36 -0500

    linting: updating config and linting rules to allow for linting to flow through vscode and apply on save

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "react";
 import * as React from "react";
 const App = () => {
-
-    return (
-        <>
-            <h1>Hello React</h1>
-            <p>Hi there! This is my first webpack site I'm building.</p>
-        </>
-    );
+  return (
+    <>
+      <h1>Hello React</h1>
+      <p>Hi there! This is my first webpack site I'm building.</p>
+      <p>This will eventually be my portfolio site!</p>
+      {/* this input should return an error because of the autoComplete attribute */}
+      <input type="text" autoComplete="failure" />
+    </>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import "./App.scss";
 
 const el = document.getElementById("app");
