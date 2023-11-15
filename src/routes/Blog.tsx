057dac7ef3a5195991d3c7806c88dc44a4843bae
Kalib Watson <kwatson@kwatson-mac-2155.local>
2023-11-15 08:34:34 -0500

    content: initial blog content structure:

--- src/routes/Blog.tsx
+++ src/routes/Blog.tsx
@@ -5,8 +5,82 @@ const Blog = () => {
   return (
     <>
       <Nav />
-      <h1>Blog</h1>
-      <p>This is the blog page!</p>
+      <main>
+        <h1>Kalib&rsquo;s Blog</h1>
+        <div role="tablist">
+          <h2>Filter posts by tags</h2>
+          <button role="tab" id="accessibility">
+            Accessibility
+          </button>
+          <button role="tab" id="development">
+            Development
+          </button>
+          <button role="tab" id="usability">
+            Usability
+          </button>
+        </div>
+        <section aria-label="blog posts" /*role log here?*/>
+          <ul>
+            <li>
+              <article>
+                <h3>Article title</h3>
+                <p>January 1, 2024</p>
+                <p>
+                  Article description will go here! Should be maybe 1-2
+                  sentences total?
+                </p>
+                <p>
+                  <span>Accessibility</span>
+                  <span>Development</span>
+                </p>
+              </article>
+            </li>
+            <li>
+              <article>
+                <h3>Article title</h3>
+                <p>January 1, 2024</p>
+                <p>
+                  Article description will go here! Should be maybe 1-2
+                  sentences total?
+                </p>
+                <p>
+                  <span>Accessibility</span>
+                  <span>Development</span>
+                </p>
+              </article>
+            </li>
+            <li>
+              <article>
+                <h3>Article title</h3>
+                <p>January 1, 2024</p>
+                <p>
+                  Article description will go here! Should be maybe 1-2
+                  sentences total?
+                </p>
+                <p>
+                  <span>Accessibility</span>
+                  <span>Development</span>
+                </p>
+              </article>
+            </li>
+            <li>
+              <article>
+                <h3>Article title</h3>
+                <p>January 1, 2024</p>
+                <p>
+                  Article description will go here! Should be maybe 1-2
+                  sentences total?
+                </p>
+                <p>
+                  <span>Accessibility</span>
+                  <span>Development</span>
+                </p>
+              </article>
+            </li>
+          </ul>
+          <button type="button">Load More Articles</button>
+        </section>
+      </main>
     </>
   );
 };
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,38 +6,40 @@ const Home = () => {
   return (
     <>
       <Nav />
-      <div id="intro">
-        <h1>
-          Hi, I&rsquo;m Kalib. I enjoy engineering intuitive, impactful, and
-          accessible digital experiences. I also{" "}
-          <Link to="blog">write about it</Link>!
-        </h1>
-      </div>
-      <div id="writing">
-        <h2>Writing</h2>
-        <ul id="article-list">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-      <div id="projects">
-        <h2>Projects</h2>
-        <ul id="project-list">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-        <p>Link to CodePen for more small examples</p>
-        <p>Link to Github for code history</p>
-      </div>
-      <div id="about">
-        <h2>About Kalib</h2>
-        <p>Some short info about myself</p>
-        <p>Link to LinkedIn</p>
-      </div>
+      <main>
+        <div id="intro">
+          <h1>
+            Hi, I&rsquo;m Kalib. I enjoy engineering intuitive, impactful, and
+            accessible digital experiences. I also{" "}
+            <Link to="blog">write about it</Link>!
+          </h1>
+        </div>
+        <div id="writing">
+          <h2>Writing</h2>
+          <ul id="article-list">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div id="projects">
+          <h2>Projects</h2>
+          <ul id="project-list">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <p>Link to CodePen for more small examples</p>
+          <p>Link to Github for code history</p>
+        </div>
+        <div id="about">
+          <h2>About Kalib</h2>
+          <p>Some short info about myself</p>
+          <p>Link to LinkedIn</p>
+        </div>
+      </main>
     </>
   );
 };
